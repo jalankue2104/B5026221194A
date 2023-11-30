643a764745b82e2f011f00d7cca11fad6bdd2172
jalankue2104 <148714351+jalankue2104@users.noreply.github.com>
2023-11-30 21:03:31 +0700

    commit file2

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel #4 : Passing Data Controller Ke View Laravel - www.malasngoding.com</title>
+</head>
+<body>
+
+	<h1>Tutorial Laravel</h1>
+	<a href="https://www.malasngoding.com/category/laravel">www.malasngoding.com</a>
+
+	<br>
+
+	<p>Nama : {{ $nama }}
+        Alamat : {{ $alamat }}
+        Umur : {{ $umur }}
+    </p>
+
+	<p>Mata Pelajaran</p>
+<a href="showjam/{{date(G:i:s)}}">Klik Disini</a>
+
+</body>
+</html>
---
+++ resources/views/tugascss.blade.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="asetugascss/stylez.css">
+</head>
+<body>
+    <a href="https://www.instagram.com/" target="_blank">Web Google</a>
+    <br>
+    <a href="https://www.instagram.com/"> Insta</a>
+    <div class="boi">
+        <img src="https://www.its.ac.id/wp-content/uploads/sites/2/2023/08/Prof-Dr-Eng-Nanik-Suciati-SKom-MKom-saat-memaparkan-orasi-ilmiahnya-pada-prosesi-pengukuhannya-sebagai-Guru-Besar-atau-Profesor-ke-161-ITS-150x150.jpeg">
+    </div>
+</body>
+</html>
