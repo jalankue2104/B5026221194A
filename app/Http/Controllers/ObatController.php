08c0bd36bf6683ce768fa41264bcc96312f03cd3
jalankue2104 <148714351+jalankue2104@users.noreply.github.com>
2023-12-11 00:55:52 +0700

    Add files via upload
    Tugas Pra EAS

---
+++ app/Http/Controllers/ObatController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class ObatController extends Controller
+{
+	public function index()
+	{
+    	// mengambil data dari table pegawai
+		// $pegawai = DB::table('pegawai')->get();
+        $obat = DB::table('obat')  ->orderBy('merkobat', 'asc')
+        ->get();
+
+
+    	// mengirim data pegawai ke view index
+		return view('index4',['obat' => $obat]);
+
+	}
+
+	// method untuk menampilkan view form tambah pegawai
+	public function tambah()
+	{
+
+		// memanggil view tambah
+		return view('tambahobat');
+
+	}
+
+	// method untuk insert data ke table pegawai
+	public function store(Request $request)
+	{
+		// insert data ke table keranjangbelanja
+		DB::table('obat')->insert([
+			'kodeobat' => $request->kodeobat,
+			'merkobat' => $request->merkobat,
+			'stockobat' => $request->stockobat,
+			'tersedia' => ($request->stockobat > 0) ? 'y' : 'n',
+		]);
+		// alihkan halaman ke halaman keranjangbelanja
+		return redirect('/obat');
+
+	}
+
+    public function edit($kodeobat)
+	{
+		// mengambil data baju berdasarkan id yang dipilih
+		$obat = DB::table('obat')->where('kodeobat',$kodeobat)->get();
+		// passing data baju yang didapat ke view edit.blade.php
+		return view('editobat',['obat' => $obat]);
+
+	}
+
+	// update data baju
+	public function update(Request $request)
+{
+    // update data baju
+    DB::table('obat')->where('kodeobat',$request->id)->update([
+        'merkobat' => $request->merkobat,
+        'stockobat' => $request->stockobat,
+        'tersedia' => ($request->stockobat > 0) ? 'y' : 'n',
+    ]);
+    // alihkan halaman ke halaman baju
+    return redirect('/obat');
+}
+
+
+	// method untuk hapus data pegawai
+	public function hapus($kodeobat)
+	{
+		// menghapus data pegawai berdasarkan id yang dipilih
+		DB::table('obat')->where('kodeobat',$kodeobat)->delete();
+
+		// alihkan halaman ke halaman pegawai
+		return redirect('/obat');
+	}
+
+    public function view($kodeobat)
+	{
+		// mengambil data pegawai berdasarkan id yang dipilih
+		$obat = DB::table('obat')->where('kodeobat',$kodeobat)->first();
+		// passing data pegawai yang didapat ke view view.blade.php
+		return view ('viewobat',['obat' => $obat]);
+
+	}
+    public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$obat = DB::table('obat')
+		            ->where('merkobat','like',"%".$cari."%")
+                    ->orderBy('merkobat', 'asc')
+                    ->paginate(10);
+
+    		// mengirim data pegawai ke view index
+		return view('index4',['obat' => $obat, 'cari' => $cari]);
+	}
+
+
+}
