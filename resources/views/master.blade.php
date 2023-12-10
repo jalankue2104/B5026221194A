9edb5763170e3de67074cbf531b93eee0c2e8886
jalankue2104 <148714351+jalankue2104@users.noreply.github.com>
2023-12-11 00:48:10 +0700

    Update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,44 +1,264 @@
-@extends('master')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title','Data Pegawai')
+<head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style class="csstambah"> .card {
+        background-color: #444;
+        border: 1px solid #555;
+        border-radius: 4px;
+        padding: 20px;
+        margin: 20px auto;
+        max-width: 600px;
+        box-shadow: 0 2px 4px rgba(0,0,0,0.5);
+    }
+    .form-group {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+    .form-group label {
+        width: 100px;
+        margin-right: 10px;
+        color: #ddd;
+    }
+    .form-group input, .form-group textarea {
+        flex: 1;
+        padding: 8px;
+        border: 1px solid #666;
+        background-color: #555;
+        color: #fff;
+         border-radius: 4px;
+    }
+    .form-group textarea {
+        resize: vertical;
+    }
+    .form-submit {
+        text-align: right;
+    }
+    .form-submit input[type="submit"] {
+        background-color: #666;
+        color: #fff;
+        border: none;
+        padding: 10px 15px;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+    }
+    .form-submit input[type="submit"]:hover {
+        background-color: #777;
+    }</style>
+    <style class="csshero">
+     .hero-container {
+  display: flex;
+  height: 470px;
+  padding: 0 10%;
+  background-color: #bdc3c7;
+}
+
+.hero-container > div {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+}
+
+.hero-container h1 {
+  font-size: 60px;
+  width: 360px;
+  margin: 20px 0;
+  font-weight: bolder;
+}
+
+.hero-container p {
+  font-size: 30px;
+  color: var(--gray);
+}
+
+.hero-container > img {
+  width: 50%;
+  height: 323px;
+  align-self: center;
+}
+    </style>
+    <style class="csstable">
+        table {
+    border: 2px;
+    background-color: white;
+    border: 1px solid #bdc3c7;
+box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2), -1px -1px 8px rgba(0, 0, 0, 0.2);
+}
 
 
-@section('konten')
-    <h3>Data Pegawai</h3>
+
+tr {
+transition: all .2s ease-in;
+cursor: pointer;
+}
+
+th{
+background-color: pink;
+}
+td {
+padding: 12px;
+text-align: left;
+border-bottom: 1px solid #ddd;
+background-color: white;
+}
+
+.isi:hover {
+background-color: whitesmoke;
+transform: scale(1.02);
+box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2), -1px -1px 8px rgba(0, 0, 0, 0.2);
+}
+.hover-effect:hover {
+    background-color: #f8f9fa;
+    transform: scale(1.02);
+}
+
+.hover-effect:hover input[disabled] {
+    color: #000;
+}
 
 
-    <a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+    </style>
+    <style class="cssform">
+    .hover-effect:hover {
+    background-color: #f8f9fa;
+    transform: scale(1.02);
+}
 
-    <br/>
-    <p>Cari Data Pegawai Berdasarkan Nama:</p>
-	<form action="/pegawai/cari" method="GET">
-		<input class="form-control fluid" type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old("cari", isset($cari) ? $cari : '') }}">
-		<input class="btn btn-primary " type="submit" value="CARI">
-	</form>
-    <br/>
+.hover-effect:hover input[disabled] {
+    color: #000;
+}
 
-    <table class="table table-striped table-hover rounded">
-        <tr class="bg-primary text-white row">
-            <th class="col-2">Nama</th>
-            <th class="col-2">Jabatan</th>
-            <th class="col-1">Umur</th>
-            <th class="col-4">Alamat</th>
-            <th class="col-3">Opsi</th>
-        </tr>
-        @foreach ($pegawai as $p)
-            <tr class="row">
-                <td class="col-2">{{ $p->pegawai_nama }}</td>
-                <td class="col-2">{{ $p->pegawai_jabatan }}</td>
-                <td class="col-1">{{ $p->pegawai_umur }}</td>
-                <td class="col-4">{{ $p->pegawai_alamat }}</td>
-                <td class="col-3">
-                    <a href="/pegawai/view/{{ $p->pegawai_id }}" class="btn btn-success">View</a>
-                    <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
-                    |
-                    <a href="/pegawai/hapus/{{ $p->pegawai_id }} " class="btn btn-danger">Hapus</a>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-    {{$pegawai->links()}}
-@endsection
+
+
+.form-control[disabled] {
+    color: #fff;
+}
+
+    </style>
+    <style class="cssnav">* {
+        margin: 0;
+        padding: 0;
+        font-family: "futura md bt";
+        text-decoration: none;
+        list-style: none;
+        box-sizing: border-box;
+    }
+    .navi {
+        background: whitesmoke;
+    }
+    header {
+        background: #fff;
+        width: 100%;
+        height: 70px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 50px;
+    }
+    header .logo {
+        font-size: 30px;
+        text-transform: uppercase;
+    }
+    header nav ul {
+        display: flex;
+    }
+    header nav ul li a {
+        display: inline-block;
+        color: #000;
+        padding: 5px 0;
+        margin: 0 10px;
+        border: 3px solid transparent;
+        text-transform: uppercase;
+        transition: 0.2s;
+    }
+    header nav ul li a:hover,
+    header nav ul li a.active {
+        border-bottom-color: dodgerblue;
+    }
+    .hamburger {
+        cursor: pointer;
+        display: none;
+    }
+    .hamburger div {
+        width: 30px;
+        height: 3px;
+        margin: 5px 0;
+        background: #000;
+    }
+    @media only screen and (max-width: 900px) {
+        header {
+            padding: 0 30px;
+        }
+    }
+    @media only screen and (max-width: 700px) {
+        .hamburger {
+            display: block;
+        }
+        header nav {
+            position: absolute;
+            width: 100%;
+            left: -100%;
+            top: 70px;
+            width: 100%;
+            background: #fff;
+            padding: 30px;
+            transition: 0.3s;
+        }
+        header #nav_check:checked ~ nav {
+            left: 0;
+        }
+        header nav ul {
+            display: block;
+        }
+        header nav ul li a {
+            margin: 5px 0;
+        }
+    }
+    </style>
+</head>
+
+<body class="navi">
+    <header>
+    <div class="logo">Arayzi Rayyansyah - 5026221194</div>
+    <input type="checkbox" id="nav_check" hidden>
+    <nav>
+        <ul>
+            <li>
+                <a href="/pegawai" >Pegawai</a>
+            </li>
+            <li>
+                <a href="/nilai">Nilai</a>
+            </li>
+            <li>
+                <a href="/keranjangbelanja">Keranjang </a>
+            </li>
+        </ul>
+    </nav>
+    <label for="nav_check" class="hamburger">
+        <div></div>
+        <div></div>
+        <div></div>
+    </label>
+</header>
+    <br>
+
+    <div class="container">
+        @yield('konten')
+    </div>
+
+    @include('footer')
+</body>
+
+</html>
